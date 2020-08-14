29adba196c12b8429ad7c3d7c365af67d1ff4f86
Raf <RafhaanShah@users.noreply.github.com>
2020-08-14 06:45:37 +0000

    Fix formatting

--- search/LinearSearch.java
+++ search/LinearSearch.java
@@ -5,8 +5,8 @@ import java.util.List;
 public class LinearSearch {
 
     public static <T> int find(List<T> list, T element) {
-        for(int i = 0; i < list.size(); i++){
-            if(element.equals(list.get(i))){
+        for (int i = 0; i < list.size(); i++) {
+            if (element.equals(list.get(i))) {
                 return i;
             }
         }
